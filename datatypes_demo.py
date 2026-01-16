3c61eed7d30aa681086eec5232966b627bf47bee
Kurra Murali Krishna <kurramurali3126@gmail.com>
2026-01-16 12:12:55 +0530

    Add files via upload

---
+++ datatypes_demo.py
@@ -0,0 +1,59 @@
+
+# This is a python int type
+
+a= 31
+print(a)
+
+b=18
+print(b)
+
+
+# This is a python float type
+
+a=3.1
+print(a)
+
+b=1.8
+print(b)
+
+
+# This is a python String type
+
+s="murali"
+print(s)
+
+#type conversation
+
+a = 5
+print(type(a)) 
+
+x="Murali"
+print(type(x))
+
+x=True
+print(type(x))
+
+
+#Arithmetic operations using numeric variables
+
+a= 100
+b=20
+c=100
+
+x=a+b+c
+print(x)
+
+x=a+b-c
+print(x)
+
+x=a*b*c
+print(x)
+
+
+
+
+
+
+
+
+
